9769cb2c6cd2d354e01c73fb35cabcd1c63b2684
Danvir Guram <danvir@Danvirs-Mac-Studio.local>
2026-02-04 18:18:10 +0000

    Add github action

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: process.env.GITHUB_PAGES === "true" ? "/devclock" : undefined,
+  assetPrefix: process.env.GITHUB_PAGES === "true" ? "/devclock/" : undefined,
 };
 
 export default nextConfig;
